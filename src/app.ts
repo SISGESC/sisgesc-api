8cf22c645957529a851208190930c0dde2c311b8
Victor Aurelio <ovictoraurelio@gmail.com>
2025-04-07 12:58:41 -0300

    feat: add Swagger UI integration for API documentation
    Implemented Swagger UI in the Express application to provide interactive API documentation at the '/api-docs' endpoint. Customized the appearance and title of the documentation interface.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
 
 import 'reflect-metadata';
 import { AppDataSource } from './database/app-data-source';
@@ -8,6 +9,7 @@ import enrollmentRouter from './routers/enrollment';
 import classGroupRouter from './routers/class-group';
 import shiftRouter from './routers/shift';
 import tuitionRouter from './routers/tuition';
+import { swaggerSpec } from './swagger';
 
 export const createExpressApp = async () => {
   try {
@@ -23,6 +25,13 @@ export const createExpressApp = async () => {
 
   app.use(cors({
     origin: ['https://quoti.cloud']
+  }));
+
+  // Swagger UI
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "SISGESC API Documentation"
   }));
 
   app.use('/', enrollmentRouter);
